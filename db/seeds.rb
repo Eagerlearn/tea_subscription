462adc8aea91f08a0cd2a36a66e1c3bd0e1059e4
craig cardwell <wizeduvcc@gmail.com>
2022-08-04 13:25:54 -0600

    add seeds data for teas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+20.times do
+  Tea.create!(
+    title: Faker::Tea.variety,
+    description: Faker::Tea.variety,
+    temperature: Faker::Number.within(range: 120..210),
+    brew_time: Faker::Number.within(range: 1..15)
+  )
+end
