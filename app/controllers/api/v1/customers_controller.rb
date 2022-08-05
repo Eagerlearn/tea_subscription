3a7c85742195d0d4a738f6a01d45e68bd158fb94
craig cardwell <wizeduvcc@gmail.com>
2022-08-05 12:17:00 -0600

    edit customer controller to add create action

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -2,4 +2,13 @@ class Api::V1::CustomersController < ApplicationController
   def index
     render json: Customer.all
   end
+
+  def create
+    render json: Customer.create!(cust_params)
+  end
+
+  private
+  def cust_params
+    params.require(:customer).permit(:first_name, :last_name, :email, :address)
+  end
 end
---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::SubscriptionsController < ApplicationController
+  # def create
+  #   render json:
+  # end
+end
