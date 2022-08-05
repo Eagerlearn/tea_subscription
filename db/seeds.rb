0d0d9045e0cc92e5201e92aa7787bcb637b485a5
craig cardwell <wizeduvcc@gmail.com>
2022-08-05 08:28:44 -0600

    add seed data for customer and edit seed file to add destroy all for tea and customer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+Tea.destroy_all
+Customer.destroy_all
 
 20.times do
   Tea.create!(
@@ -13,5 +14,13 @@
     description: Faker::Tea.variety,
     temperature: Faker::Number.within(range: 120..210),
     brew_time: Faker::Number.within(range: 1..15)
+  )
+end
+20.times do
+  Customer.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.first_name,
+    email: Faker::Internet.email,
+    address: Faker::Address.full_address
   )
 end
