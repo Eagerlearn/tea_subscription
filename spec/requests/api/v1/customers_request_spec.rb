fd1d398305f23ec81a3419bb273831b7e7f308f1
craig cardwell <wizeduvcc@gmail.com>
2022-08-05 08:15:59 -0600

    add request test and faker data for customers

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -7,5 +7,26 @@ describe "Customers API" do
     get '/api/v1/customers'
 
     expect(response).to be_successful
+
+    customers = JSON.parse(response.body, symbolize_names: true)
+
+    expect(customers.count).to eq(3)
+
+    customers.each do |customer|
+      expect(customer).to have_key(:id)
+      expect(customer[:id]).to be_an(Integer)
+
+      expect(customer).to have_key(:first_name)
+      expect(customer[:first_name]).to be_an(String)
+
+      expect(customer).to have_key(:last_name)
+      expect(customer[:last_name]).to be_an(String)
+
+      expect(customer).to have_key(:email)
+      expect(customer[:email]).to be_an(String)
+
+      expect(customer).to have_key(:address)
+      expect(customer[:address]).to be_an(String)
+    end
   end
 end
